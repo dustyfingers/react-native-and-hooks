8a16674bea16169a004e1e6b0154eeb16c1fd2e0
LouieW <dustyfingers01257@gmail.com>
2022-01-01 00:22:51 -0700

    completed starter challenge

--- rn-starter/src/screens/ComponentsScreen.js
+++ rn-starter/src/screens/ComponentsScreen.js
@@ -3,19 +3,22 @@ import { Text, StyleSheet, View } from 'react-native';
 
 
 const styles = StyleSheet.create({
-    textStyle: {
-        fontSize: 30
+    headerStyle: {
+        fontSize: 45
+    },
+    subheaderStyle: {
+        fontSize: 20
     }
 });
 
 const ComponentsScreen = () => {
-    
-    const greeting = "Bye there!";
+
+    const myName = "Louie";
 
     return (
         <View>
-            <Text style={styles.textStyle}>This is the components screen!</Text>
-            <Text>{greeting}</Text>
+            <Text style={styles.headerStyle}>Getting started with React Native!</Text>
+            <Text style={styles.subheaderStyle}>{`My name is ${myName}!`}</Text>
         </View>
     );
 
