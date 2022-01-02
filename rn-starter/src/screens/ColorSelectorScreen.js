13e1b0bed78c19e502f0a9833ad464f074898eb8
LouieW <dustyfingers01257@gmail.com>
2022-01-02 14:17:53 -0700

    refactored into handleColorChange 'reducer' function, made view bigger

--- rn-starter/src/screens/ColorSelectorScreen.js
+++ rn-starter/src/screens/ColorSelectorScreen.js
@@ -4,6 +4,8 @@ import { View, StyleSheet } from 'react-native';
 import ColorCounter from '../components/ColorCounter';
 
 
+const COLOR_INCREMENT = 9;
+
 const styles = StyleSheet.create({});
 
 const ColorSelectorScreen = () => {
@@ -12,9 +14,36 @@ const ColorSelectorScreen = () => {
     const [green, setGreen] = useState(0);
     const [blue, setBlue] = useState(0);
 
+    // check if state is updating correctly
     useEffect(() => {
         console.log(`red: ${red}, green: ${green}, blue: ${blue}`);
     }, [red, green, blue]);
+
+    const handleColorChange = (color, add) => {
+
+        switch (color) {
+
+            case "Red":
+                if (add) setRed(Math.min(red + COLOR_INCREMENT, 255));
+                else setRed(Math.max(red - COLOR_INCREMENT, 0));
+                break;
+
+            case "Green":
+                if (add) setGreen(Math.min(green + COLOR_INCREMENT, 255));
+                else setGreen(Math.max(green - COLOR_INCREMENT, 0));
+                break;
+
+            case "Blue":
+                if (add) setBlue(Math.min(blue + COLOR_INCREMENT, 255));
+                else setBlue(Math.max(blue - COLOR_INCREMENT, 0));
+                break;
+
+            default:
+                break;
+
+        }
+
+    };
 
     return (
 
@@ -22,20 +51,20 @@ const ColorSelectorScreen = () => {
 
             <ColorCounter 
                 color="Red" 
-                handleIncrease={() => setRed(Math.min(red + 12, 255))}
-                handleDecrease={() => setRed(Math.max(red - 12, 0))} />
+                handleIncrease={() => handleColorChange('Red', 1)}
+                handleDecrease={() => handleColorChange('Red', 0)} />
 
             <ColorCounter 
                 color="Green" 
-                handleIncrease={() => setGreen(Math.min(green + 12, 255))}
-                handleDecrease={() => setGreen(Math.max(green - 12, 0))} />
+                handleIncrease={() => handleColorChange('Green', 1)}
+                handleDecrease={() => handleColorChange('Green', 0)} />
 
             <ColorCounter 
                 color="Blue"
-                handleIncrease={() => setBlue(Math.min(blue + 12, 255))}
-                handleDecrease={() => setBlue(Math.max(blue - 12, 0))} />
+                handleIncrease={() => handleColorChange('Blue', 1)}
+                handleDecrease={() => handleColorChange('Blue', 0)} />
 
-            <View style={{ width: 100, height: 100, backgroundColor: `rgb(${red}, ${green}, ${blue})`}}/>
+            <View style={{ width: 150, height: 150, backgroundColor: `rgb(${red}, ${green}, ${blue})`}}/>
 
         </View>
 
