6ed0f5b48e14104ee9447ed8952a11ce9023063a
LouieW <dustyfingers01257@gmail.com>
2022-01-03 21:00:46 -0700

    refactored state logic for color selector screen using reacts useReducer hook!

--- rn-starter/src/screens/ColorSelectorScreen.js
+++ rn-starter/src/screens/ColorSelectorScreen.js
@@ -1,49 +1,74 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useReducer } from 'react';
 import { View, StyleSheet } from 'react-native';
 
 import ColorCounter from '../components/ColorCounter';
+import { clamp } from '../helpers/tools';
 
 
-const COLOR_INCREMENT = 9;
+const COLOR_INCREMENT = 20;
 
 const styles = StyleSheet.create({});
 
+const reducer = (state, action) => {
+    
+    switch (action.color) 
+    {
+
+        case 'red':
+            return {...state, red: clamp(state.red + action.amount, 0 , 255)};
+
+        case 'green':
+            return {...state, green: clamp(state.green + action.amount, 0 , 255)};
+
+        case 'blue':
+            return {...state, blue: clamp(state.blue + action.amount, 0 , 255)};
+
+        default:
+            return state;
+
+    }
+
+};
+
 const ColorSelectorScreen = () => {
 
-    const [red, setRed] = useState(0);
-    const [green, setGreen] = useState(0);
-    const [blue, setBlue] = useState(0);
+    const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
 
-    // check if state is updating correctly
-    useEffect(() => {
-        console.log(`red: ${red}, green: ${green}, blue: ${blue}`);
-    }, [red, green, blue]);
+    // these exist in 'state' above now
+    // const [red, setRed] = useState(0);
+    // const [green, setGreen] = useState(0);
+    // const [blue, setBlue] = useState(0);
 
-    const handleColorChange = (color, add) => {
+    // all this nastly logic is contained in the reducer above now!
+    // much cleaner, isnt it?
+    // const handleColorChange = (color, add) => {
 
-        switch (color) {
+    //     switch (color) {
 
-            case "Red":
-                if (add) setRed(Math.min(red + COLOR_INCREMENT, 255));
-                else setRed(Math.max(red - COLOR_INCREMENT, 0));
-                break;
+    //         case "Red":
+    //             if (add) setRed(Math.min(red + COLOR_INCREMENT, 255));
+    //             else setRed(Math.max(red - COLOR_INCREMENT, 0));
+    //             break;
 
-            case "Green":
-                if (add) setGreen(Math.min(green + COLOR_INCREMENT, 255));
-                else setGreen(Math.max(green - COLOR_INCREMENT, 0));
-                break;
+    //         case "Green":
+    //             if (add) setGreen(Math.min(green + COLOR_INCREMENT, 255));
+    //             else setGreen(Math.max(green - COLOR_INCREMENT, 0));
+    //             break;
 
-            case "Blue":
-                if (add) setBlue(Math.min(blue + COLOR_INCREMENT, 255));
-                else setBlue(Math.max(blue - COLOR_INCREMENT, 0));
-                break;
+    //         case "Blue":
+    //             if (add) setBlue(Math.min(blue + COLOR_INCREMENT, 255));
+    //             else setBlue(Math.max(blue - COLOR_INCREMENT, 0));
+    //             break;
 
-            default:
-                break;
+    //         default:
+    //             break;
 
-        }
+    //     }
 
-    };
+    // };
+
+    // watch state changes
+    useEffect(() => console.log(state), [state]);
 
     return (
 
@@ -51,20 +76,20 @@ const ColorSelectorScreen = () => {
 
             <ColorCounter 
                 color="Red" 
-                handleIncrease={() => handleColorChange('Red', 1)}
-                handleDecrease={() => handleColorChange('Red', 0)} />
+                handleIncrease={() => dispatch({color: 'red', amount: COLOR_INCREMENT})}
+                handleDecrease={() => dispatch({color: 'red', amount: COLOR_INCREMENT * -1})} />
 
             <ColorCounter 
                 color="Green" 
-                handleIncrease={() => handleColorChange('Green', 1)}
-                handleDecrease={() => handleColorChange('Green', 0)} />
+                handleIncrease={() => dispatch({color: 'green', amount: COLOR_INCREMENT})}
+                handleDecrease={() => dispatch({color: 'green', amount: COLOR_INCREMENT * -1})} />
 
             <ColorCounter 
                 color="Blue"
-                handleIncrease={() => handleColorChange('Blue', 1)}
-                handleDecrease={() => handleColorChange('Blue', 0)} />
+                handleIncrease={() => dispatch({color: 'blue', amount: COLOR_INCREMENT})}
+                handleDecrease={() =>dispatch({color: 'blue', amount: COLOR_INCREMENT * -1})} />
 
-            <View style={{ width: 150, height: 150, backgroundColor: `rgb(${red}, ${green}, ${blue})`}}/>
+            <View style={{ width: 150, height: 150, backgroundColor: `rgb(${state.red}, ${state.green}, ${state.blue})`}}/>
 
         </View>
 
