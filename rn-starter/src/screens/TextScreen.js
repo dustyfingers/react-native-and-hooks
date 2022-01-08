aaf4fdfd9020c1acd42c5fa9edc5a70982e94483
LouieW <dustyfingers01257@gmail.com>
2022-01-07 20:34:27 -0700

    make text screen stateful

--- rn-starter/src/screens/TextScreen.js
+++ rn-starter/src/screens/TextScreen.js
@@ -1,27 +1,45 @@
-import React from 'react';
-import { View, TextInput, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, TextInput, Text, StyleSheet } from 'react-native';
 
 
 const styles = StyleSheet.create({
+
     input: {
         margin: 15,
         borderColor: 'black',
         borderWidth: 1
     }
+
 });
 
 const TextScreen = () => {
+
+    const [text, setText] = useState('');
+
+    // check for proper state change 
+    useEffect(() => console.log(text), [text]);
+
     return (
+
         <View>
+
             {/* text input works a lil differently in react native! */}
             {/* by default on ios, the first letter of an input is capitalized! the autoCapitalize prop prevents this behavior */}
             {/* autoCorrect=false prevents the smae but with auto correct */}
+
             <TextInput 
                 autoCapitalize='none'
                 autoCorrect={false}
-                style={styles.input} />
+                style={styles.input}
+                value={text}
+                onChangeText={val => setText(val)} />
+
+            <Text>{text}</Text>
+
         </View>
+
     );
+
 };
 
 export default TextScreen;
