5928db331c8afe352522ac239223a6ef94a7c56f
LouieW <dustyfingers01257@gmail.com>
2022-01-07 20:56:23 -0700

    started build of box model demo screen

--- rn-starter/src/screens/BoxScreen.js
+++ rn-starter/src/screens/BoxScreen.js
@@ -2,13 +2,23 @@ import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-
+    viewStyle: {
+        borderWidth: 1,
+        borderColor: 'black'
+    },
+    textStyle: {
+        // marginVertical: 20,
+        // marginHorizontal: 15,
+        margin: 20,
+        borderWidth: 1,
+        borderColor: 'red'
+    }
 });
 
 const BoxScreen = () => {
     return (
-        <View>
-            <Text>Box Screen</Text>
+        <View style={styles.viewStyle}>
+            <Text style={styles.textStyle}>Box Screen</Text>
         </View>
     );
 };
