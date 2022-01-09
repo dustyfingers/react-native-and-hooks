41eb04e77781fa2a47ae3af1cc7f4cfac0b24d99
LouieW <dustyfingers01257@gmail.com>
2022-01-09 13:17:33 -0700

    styled box screen demo using react natives 'flexbox' styling

--- rn-starter/src/screens/BoxScreen.js
+++ rn-starter/src/screens/BoxScreen.js
@@ -4,12 +4,28 @@ import { Text, View, StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
     viewStyle: {
         borderWidth: 1,
-        borderColor: 'black'
+        borderColor: 'black',
+        // default is alignItems: 'stretch'
+        // alignItems: 'center',
+        // default is flexDirection: 'column'
+        // flexDirection: 'row',
+        // default is justifyContent: 'flex-start'
+        // justifyContent: 'center',
+        height: 250
     },
-    textStyle: {
-        // marginVertical: 20,
-        // marginHorizontal: 15,
-        margin: 20,
+    textOneStyle: {
+        borderWidth: 1,
+        borderColor: 'red'
+    },
+    textTwoStyle: {
+        alignSelf: 'center',
+        flex: 1,
+        borderWidth: 1,
+        borderColor: 'red'
+    },
+    textThreeStyle: {
+        alignSelf: 'flex-end',
+        flex: 1,
         borderWidth: 1,
         borderColor: 'red'
     }
@@ -18,7 +34,9 @@ const styles = StyleSheet.create({
 const BoxScreen = () => {
     return (
         <View style={styles.viewStyle}>
-            <Text style={styles.textStyle}>Box Screen</Text>
+            <Text style={styles.textOneStyle}>Child #1</Text>
+            <Text style={styles.textTwoStyle}>Child #2</Text>
+            <Text style={styles.textThreeStyle}>Child #3</Text>
         </View>
     );
 };
